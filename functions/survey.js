7c53a1812391db0fc32da99e4ea3ee0d3d691f0e
Adnan Khan <adnan7.zain@gmail.com>
2021-12-18 18:40:15 +0500

    fetch votes

--- functions/survey.js
+++ functions/survey.js
@@ -1,6 +1,27 @@
+require('dotenv').config();
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appzY7TorthrOd3qS')
+  .table('survey');
+
 exports.handler = async (event, context, cd) => {
-  return {
-    statusCode: 200,
-    body: 'survey project',
-  };
+  try {
+    const { records } = await airtable.list();
+    const survey = records.map((item) => {
+      const { id } = item;
+      const { votes, room } = item.fields;
+      return { id, room, votes };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(survey),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Server Error',
+    };
+  }
 };
